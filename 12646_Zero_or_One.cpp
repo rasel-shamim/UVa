40affb4bb8c1c58ba575f4653c574710acd6a622
rasel-shamim <60275888+rasel-shamim@users.noreply.github.com>
2020-03-08 01:07:14 +0600

    Create 12646_Zero_or_One.cpp

---
+++ 12646_Zero_or_One.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    char x;
+    while(scanf("%d %d %d",&a,&b,&c)!=EOF){
+        if((a==1 && b==0 && c==0) || (a==0 && b==1 && c==1)){
+            x='A';
+        }
+        else if((b==1 && a==0 && c==0) || (b==0 && a==1 && c==1)){
+            x='B';
+        }
+        else if((c==1 && a==0 && b==0) || (c==0 && a==1 && b==1)){
+            x='C';
+        }
+        else if((a==0 && b==0 && c==0 ) || (a==1 && b==1 && c==1)){
+            x='*';
+        }
+        printf("%c\n",x);
+    }
+    return 0;
+}
