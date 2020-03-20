888cd397d79e254af7553a98e3be7ae1f80189df
Shamim Ahammad Rasel <60275888+rasel-shamim@users.noreply.github.com>
2020-03-20 23:21:13 +0600

    Create 575-Skew_Binary.cpp

---
+++ 575-Skew_Binary.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stdio.h>
+#include<cmath>
+#include<cstring>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+   ll result =0;
+
+   char number[100];
+
+   while(scanf("%s",number)!=EOF){
+
+
+      ll len,i,x,cnt =0;
+
+      len = strlen(number);
+      if(len==1 && number[0] == '0')
+         break;
+
+      for(i=len-1;i>=0;i--){
+         x = (int)number[i] - 48;
+         result += x*(pow(2,cnt+1) -1);
+         cnt++;
+      }
+
+      cout<<result<<endl;
+      cnt = 0;
+      result =0;
+   }
+   return 0;
+}
