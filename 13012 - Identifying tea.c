5fea19e62a0c1a6eeec5484d956fd941a5ee25e2
Shamim Ahammad Rasel <60275888+rasel-shamim@users.noreply.github.com>
2020-07-12 00:09:10 +0600

    Update 13012 - Identifying tea.c

--- 13012 - Identifying tea.c
+++ 13012 - Identifying tea.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-	int T;
+  int T;
     
   while(scanf("%d",&T)==1){
         int x,count =0;
-        for(i=0;i<5;i++){
+        for(int i=0;i<5;i++){
             scanf("%d",&x);
             if(x==T)
                 count++;
