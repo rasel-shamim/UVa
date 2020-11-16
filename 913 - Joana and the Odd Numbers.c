8b797aa8330863654bd392a2939442ab07920ecf
Shamim Ahammad Rasel <60275888+rasel-shamim@users.noreply.github.com>
2020-11-16 21:29:36 +0600

    Create 913 - Joana and the Odd Numbers.c

---
+++ 913 - Joana and the Odd Numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	long long n;
+    while(scanf("%lld",&n)!=EOF){
+        if(n==1){
+            printf("1\n");
+        }
+        else{
+            long long temp = n/2+1;
+            temp*=temp;
+            temp = 2*temp-1;
+            long long ans = temp*3 -6;
+            printf("%lld\n",ans );
+        }
+    }
+	
+	
+return 0;
+}
