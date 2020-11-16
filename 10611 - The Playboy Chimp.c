cc24f34352dee015ea55af6d981230cbd9ced298
Shamim Ahammad Rasel <60275888+rasel-shamim@users.noreply.github.com>
2020-11-16 21:41:18 +0600

    Update 10611 - The Playboy Chimp.c

--- 10611 - The Playboy Chimp.c
+++ 10611 - The Playboy Chimp.c
@@ -4,17 +4,17 @@ int main()
 {
 	int n,i;
 	scanf("%d",&n);
-	int arr[n];
-	for(int i=0;i<n;i++){
+	int arr[n],i,j;
+	for(i=0;i<n;i++){
 		scanf("%d",&arr[i]);
 		
 	}
 	int x,temp;
 	scanf("%d",&x);
-	for(int i=0;i<x;i++){
+	for(i=0;i<x;i++){
 		scanf("%d",&temp);
 		int res=0;
-		for(int j=n-1;j>=0;j--){
+		for(j=n-1;j>=0;j--){
 			if(arr[j]<temp){
 				res = arr[j];
 				break;
@@ -27,7 +27,7 @@ int main()
 			printf("X ");
 		}
 		res = 0;
-		for(int j=0;j<n;j++){
+		for(j=0;j<n;j++){
 			if(arr[j]>temp){
 				res = arr[j];
 				break;
